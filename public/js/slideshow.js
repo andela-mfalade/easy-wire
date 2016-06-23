e9ecba502d942ea845d56330ad8aaeafb4716c89
andela-mfalade <mayowa.falade@andela.com>
2016-06-23 14:33:28 +0100

    Move all images to cloudinary

---
+++ public/js/slideshow.js
@@ -0,0 +1,10 @@
+$("#introArea").vegas({
+    delay: 8000,
+    shuffle: true,
+    overlay: true,
+    timer: false,
+    slides: [
+        { src: "http://res.cloudinary.com/doy1ofzki/image/upload/v1466683160/bg02_zcqlzj.jpg" },
+        { src: "http://res.cloudinary.com/doy1ofzki/image/upload/v1466682685/bgimg01_clkd3f.jpg" }
+    ]
+});
