a79c2bcaf327c98cd6cd14f1b9fcf99a212b7440
andela-mfalade <mayowa.falade@andela.com>
2016-08-03 12:09:57 +0100

    Change sidebar icons.. [WIP] signup and login modal in progress

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,9 +1,26 @@
 $('a[href^="#"]').on('click', function(event) {
     var target = $(this.getAttribute('href'));
+    console.log(target, 'tar');
     if( target.length ) {
         event.preventDefault();
+        target.show();
         $('html, body').stop().animate({
             scrollTop: target.offset().top
-        }, 1000);
+        }, 700);
     }
 });
+
+
+console.log("Ths is the custom js script in action..");
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+      console.log(this.nextElementSibling);
+      this.classList.toggle("active");
+      this.nextElementSibling.classList.toggle("show");
+  }
+}
